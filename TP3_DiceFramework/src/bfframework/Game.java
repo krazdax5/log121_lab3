2324d957916bfd5657c201ce65f47c8621afc6d8
Charles Levesque <c_levesque@live.ca>
2013-11-07 18:53:56 -0500

    ajout des méthodes pour les classes Game et MasterCreator.

--- TP3_DiceFramework/src/bfframework/Game.java
+++ TP3_DiceFramework/src/bfframework/Game.java
@@ -1,10 +1,56 @@
 package bfframework;
 
 /**
- * Classe abstraite de bfframework.Game
+ * Classe qui permet la communication entre les classes.
  *
- * @author Mathieu Lachance
+ *        Historique des modifications
+ ************************************************
+ * @author Mathieu Lachance LACM14059305
  * 2013-11-01 : Creation de la classe
+ * @author Charles Levesque LEVC27109107
+ * 2013-11-07 : Ajout des méthode.
  */
-public class Game {
+public abstract class Game {
+    private PlayerManager playerManager;
+    private DiceManager diceManager;
+    private Rules rules;
+
+    /**
+     * Constructeur d'un jeu.
+     * @param numPlayers Le nombre de joueurs
+     * @param numDices Le nombre de dés
+     * @param numFacesPerDice Le nombre de faces par dé
+     */
+    public Game(int numPlayers, int numDices, int numFacesPerDice){
+
+    }
+
+    /**
+     * Calcule le pointage de la ronde.
+     * @return Le pointage de la ronde.
+     */
+    public int calculateScoreRound(){
+        return -1;
+    }
+
+    /**
+     * Joue la ronde pour tous les joueurs.
+     */
+    public void playRound(){
+    }
+
+    /**
+     * Commence la partie.
+     */
+    public void startGame(){
+
+    }
+
+    /**
+     * Trouve le gagnant de la partie.
+     * @return Valeur nulle si non trouvé, le joueur si trouvé.
+     */
+    public Player findWinner(){
+        return null;
+    }
 }
