37816d527c08feb14d34c82d7d16890f9b29dfd7
Charles Levesque <c_levesque@live.ca>
2013-11-10 21:26:53 -0500

    Implémentation de Game et ajout des tests

--- TP3_DiceFramework/src/bfframework/Game.java
+++ TP3_DiceFramework/src/bfframework/Game.java
@@ -9,8 +9,10 @@ package bfframework;
  * 2013-11-01 : Creation de la classe
  * @author Charles Levesque LEVC27109107
  * 2013-11-07 : Ajout des méthode.
+ * 2013-11-10 : Implémentation des méthodes
  */
 public abstract class Game {
+    private GameStates gameState = GameStates.EARLY;
     private PlayerManager playerManager;
     private DiceManager diceManager;
     private Rules rules;
@@ -21,8 +23,22 @@ public abstract class Game {
      * @param numDices Le nombre de dés
      * @param numFacesPerDice Le nombre de faces par dé
      */
-    public void createGame(int numPlayers, int numDices, int numFacesPerDice){
+    public void createGame(int numPlayers, int numDices, int numFacesPerDice, Rules rules){
+        playerManager = new PlayerManager();
+        diceManager = new DiceManager();
 
+        while (numPlayers > 0){
+            playerManager.addPlayer(new Player());
+            numPlayers--;
+        }
+
+        while (numDices > 0){
+            diceManager.addDice(new Dice(numFacesPerDice));
+            numDices--;
+        }
+
+        this.rules = rules;
+        gameState = GameStates.CREATED;
     }
 
     /**
@@ -30,20 +46,22 @@ public abstract class Game {
      * @return Le pointage de la ronde.
      */
     public int calculateScoreRound(){
-        return -1;
+        return rules.calculateScore();
     }
 
     /**
      * Joue la ronde pour tous les joueurs.
      */
     public void playRound(){
+        Player playingPlayer = playerManager.nextPlayer();
+        rules.playRound();
     }
 
     /**
      * Commence la partie.
      */
     public void startGame(){
-
+        gameState = GameStates.STARTED;
     }
 
     /**
@@ -51,6 +69,6 @@ public abstract class Game {
      * @return Valeur nulle si non trouvé, le joueur si trouvé.
      */
     public Player findWinner(){
-        return null;
+        return rules.findWinner();
     }
 }
---
+++ TP3_DiceFramework/src/tests/GameTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.junit.Test;
+import bfframework.Game;
+
+/**
+ * Classe de tests unitaires qui teste toutes les methodes de la classe Game.
+ *
+ * @author Charles Levesque LEVC27109107
+ * 2013-11-10 : ajout des tests
+ */
+public class GameTest {
+    @Test
+    public void testCreateGame() throws Exception{
+
+    }
+
+    @Test
+    public void testCalculateScoreRound() throws Exception{
+
+    }
+
+    @Test
+    public void testPlayRound() throws Exception{
+
+    }
+
+    @Test
+    public void testStartGame() throws Exception{
+
+    }
+
+    @Test
+    public void testFindWinner() throws Exception{
+
+    }
+}
